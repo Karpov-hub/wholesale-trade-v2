05ceaf7182c72c6f42e383345c4cb94e6122227b
Karpov <mk8385873@gmail.com>
2023-06-18 17:19:05 +0300

    added seeds and corrected the filter

--- BE/packages/db/seeders/20233101105041-add-products.js
+++ BE/packages/db/seeders/20233101105041-add-products.js
@@ -158,6 +158,349 @@ module.exports = {
         description:
           "Гантели - идеальный инструмент для занятий спортом и укрепления мышц. Изготовлены из высококачественных материалови имеют удобную форму для хвата.",
       },
+
+      {
+        id: "d6280a6a-71d4-4a61-b512-ec3b057d5d69",
+        name: "Кроссовки Nike Air Max",
+        category_id: "a743d6a2-4a9b-45f7-a716-1c9b7f05693a",
+        user_id: "99313318-d59d-4e1f-b218-3c81d2a01395",
+        price: "8990",
+        discount: 0,
+        reviews: "[]",
+        rating: 4,
+        product_quantity: 30,
+        image:
+          '["https://avatars.mds.yandex.net/get-mpic/7473874/img_id7128049712297677443.jpeg/orig"]',
+        description: "Удобные и стильные кроссовки для активного образа жизни.",
+      },
+      {
+        id: "73d1b6a0-8e09-4d81-b0e2-fdfe217b60d2",
+        name: "Фен Braun Satin Hair 7",
+        category_id: "18a94f16-17d7-4a3e-b00c-591aae6d257a",
+        user_id: "7fe6128c-60c6-4af9-bc89-9c12d9ef5e5d",
+        price: "2990",
+        discount: 20,
+        reviews: "[]",
+        rating: 4,
+        product_quantity: 20,
+        image:
+          '["https://avatars.mds.yandex.net/get-mpic/5284145/img_id9069686074374442797.jpeg/orig"]',
+        description:
+          "Мощный фен с различными режимами и насадками для создания причесок.",
+      },
+      {
+        id: "10c6f60f-4ff0-4a8d-9df9-bd2bbedba5c6",
+        name: "Велосипед Stels Navigator 600",
+        category_id: "ef4789f5-3bb3-4ea7-8d78-295ea295b5ab",
+        user_id: "f6db3344-16b0-47ef-b6e0-ae0ce69b8bbd",
+        price: "21990",
+        discount: 15,
+        reviews: "[]",
+        rating: 4,
+        product_quantity: 10,
+        image:
+          '["https://avatars.mds.yandex.net/get-mpic/5239537/img_id5247125343850062423.jpeg/orig"]',
+        description: "Универсальный велосипед для активного отдыха и спорта.",
+      },
+
+      {
+        id: "c579b99a-2b25-4c94-9a2c-7ad91ad8d5e0",
+        name: "Ноутбук Dell XPS 13",
+        category_id: "ce1eb93d-22e1-4a4f-8e99-2adc7496a234",
+        user_id: "44b75e7f-07c0-43be-aa93-757f771bbd45",
+        price: "84990",
+        discount: 10,
+        reviews:
+          '[{"id_user": "9cfa5aa3-168b-4d25-bb01-8ea44a593eb7", "value": 9, "comment": "Отличное качество звука!"}]',
+        rating: 4,
+        product_quantity: 50,
+        image:
+          '["https://avatars.mds.yandex.net/get-mpic/4304063/img_id6537065206174110379.jpeg/orig"]',
+        description:
+          "Ноутбук Dell XPS 13 - компактный и мощный ноутбук с высоким разрешением дисплея и продолжительным временем работы от аккумулятора.",
+      },
+      {
+        id: "fb39e998-3e8e-4ef9-9143-4b4f7002856a",
+        name: "Телевизор LG OLED CX",
+        category_id: "ce1eb93d-22e1-4a4f-8e99-2adc7496a234",
+        user_id: "99313318-d59d-4e1f-b218-3c81d2a01395",
+        price: "79990",
+        discount: 20,
+        reviews:
+          '[{"id_user": "9cfa5aa3-168b-4d25-bb01-8ea44a593eb7", "value": 9, "comment": "Отличное качество звука!"}]',
+        rating: 4,
+        product_quantity: 30,
+        image:
+          '["https://avatars.mds.yandex.net/get-mpic/4606255/img_id6731304619691134514.jpeg/orig"]',
+        description:
+          "Телевизор LG OLED CX - высококачественный телевизор с технологией OLED и умными функциями.",
+      },
+      {
+        id: "77faf7d1-89fc-41fa-83f3-621b4b944a61",
+        name: "Наушники Sony WH-1000XM4",
+        category_id: "ce1eb93d-22e1-4a4f-8e99-2adc7496a234",
+        user_id: "99313318-d59d-4e1f-b218-3c81d2a01395",
+        price: "24990",
+        discount: 15,
+        reviews:
+          '[{"id_user": "9cfa5aa3-168b-4d25-bb01-8ea44a593eb7", "value": 9, "comment": "Отличное качество звука!"}]',
+        rating: 4,
+        product_quantity: 40,
+        image:
+          '["https://avatars.mds.yandex.net/get-mpic/4386141/img_id3192445659604357803.jpeg/orig"]',
+        description:
+          "Наушники Sony WH-1000XM4 - шумоподавляющие беспроводные наушники с высоким качеством звука и долгим временем работы от аккумулятора.",
+      },
+      {
+        id: "4b408a11-41f0-4059-a447-c8524cc7bd0d",
+        name: "Смартфон Samsung Galaxy S21",
+        category_id: "ce1eb93d-22e1-4a4f-8e99-2adc7496a234",
+        user_id: "44b75e7f-07c0-43be-aa93-757f771bbd45",
+        price: "69990",
+        discount: 5,
+        reviews:
+          '[{"id_user": "9cfa5aa3-168b-4d25-bb01-8ea44a593eb7", "value": 9, "comment": "Отличное качество звука!"}]',
+        rating: 4,
+        product_quantity: 100,
+        image:
+          '["https://avatars.mds.yandex.net/get-mpic/5233681/img_id6187862100290802397.jpeg/orig"]',
+        description:
+          "Смартфон Samsung Galaxy S21 - мощный и стильный смартфон с высококачественным дисплеем и продвинутыми камерами.",
+      },
+      {
+        id: "765656c8-4bf2-4084-9d27-bddd90018909",
+        name: "Ноутбук MacBook Pro 16",
+        category_id: "ce1eb93d-22e1-4a4f-8e99-2adc7496a234",
+        user_id: "99313318-d59d-4e1f-b218-3c81d2a01395",
+        price: "129990",
+        discount: 0,
+        reviews:
+          '[{"id_user": "9cfa5aa3-168b-4d25-bb01-8ea44a593eb7", "value": 9, "comment": "Отличное качество звука!"}]',
+        rating: 4,
+        product_quantity: 50,
+        image:
+          '["https://avatars.mds.yandex.net/get-mpic/5242361/img_id2420073412246465223.jpeg/orig"]',
+        description:
+          "Ноутбук MacBook Pro 16 - мощный и производительный ноутбук от Apple с высококачественным дисплеем Retina и продолжительным временем работы от аккумулятора.",
+      },
+      {
+        id: "76ce9d7a-0b88-4a51-90d3-7b27df7e0be9",
+        name: "Кофемашина De'Longhi Magnifica S",
+        category_id: "8d112c6b-2347-41f7-8ee5-5f8edf2a9e9f",
+        user_id: "7fe6128c-60c6-4af9-bc89-9c12d9ef5e5d",
+        price: "29990",
+        discount: 10,
+        reviews: "[]",
+        rating: 4,
+        product_quantity: 30,
+        image:
+          '["https://avatars.mds.yandex.net/get-mpic/5234850/img_id4914036461766534550.jpeg/orig"]',
+        description:
+          "Кофемашина De'Longhi Magnifica S - автоматическая кофемашина с широким набором функций и возможностью приготовления различных видов кофе.",
+      },
+      {
+        id: "a8409d2b-444e-4d2e-88e0-9073ec2caf43",
+        name: "Смарт-часы Apple Watch Series 6",
+        category_id: "ce1eb93d-22e1-4a4f-8e99-2adc7496a234",
+        user_id: "7fe6128c-60c6-4af9-bc89-9c12d9ef5e5d",
+        price: "24990",
+        discount: 0,
+        reviews:
+          '[{"id_user": "9cfa5aa3-168b-4d25-bb01-8ea44a593eb7", "value": 9, "comment": "Отличное качество звука!"}]',
+        rating: 4,
+        product_quantity: 50,
+        image:
+          '["https://avatars.mds.yandex.net/get-mpic/5279750/img_id7422563259148097831.jpeg/orig"]',
+        description:
+          "Смарт-часы Apple Watch Series 6 - элегантные и функциональные умные часы с большим набором возможностей и датчиками для отслеживания физической активности.",
+      },
+      {
+        id: "0b297426-0dad-43cf-b9f2-71f54d867511",
+        name: "Футбольный мяч Adidas",
+        category_id: "032d66d2-1be4-4f7b-967b-6df82de5df1b",
+        user_id: "44b75e7f-07c0-43be-aa93-757f771bbd45",
+        price: "1999",
+        discount: 0,
+        reviews: "[]",
+        rating: 4,
+        product_quantity: 50,
+        image:
+          '["https://avatars.mds.yandex.net/get-marketpic/6099605/picde65eea30f194428588091ef3ff774e3/orig"]',
+        description:
+          "Футбольный мяч Adidas - высококачественный мяч для игры в футбол с отличной мягкостью и контролем.",
+      },
+      {
+        id: "a144b6a6-6201-4184-adeb-55bd7e3c59b5",
+        name: "Шоколад Lindt Excellence",
+        category_id: "efb6ef33-5ae9-412b-a635-82b2e97f7480",
+        user_id: "99313318-d59d-4e1f-b218-3c81d2a01395",
+        price: "499",
+        discount: 15,
+        reviews: "[]",
+        rating: 4,
+        product_quantity: 100,
+        image:
+          '["https://avatars.mds.yandex.net/get-mpic/4984138/img_id8389734805929468919.jpeg/orig"]',
+        description:
+          "Шоколад Lindt Excellence - изысканный шоколад премиум-класса с разнообразными вкусами и шелковистой текстурой.",
+      },
+      {
+        id: "3226cd92-203f-4a3b-9b26-2b75e0aa9bc9",
+        name: "Наушники Sony WH-1000XM4",
+        category_id: "ce1eb93d-22e1-4a4f-8e99-2adc7496a234",
+        user_id: "7fe6128c-60c6-4af9-bc89-9c12d9ef5e5d",
+        price: "24990",
+        discount: 10,
+        reviews:
+          '[{"id_user": "9cfa5aa3-168b-4d25-bb01-8ea44a593eb7", "value": 9, "comment": "Отличное качество звука!"}]',
+        rating: 4,
+        product_quantity: 30,
+        image:
+          '["https://avatars.mds.yandex.net/get-mpic/4080967/img_id8128133420125143221.jpeg/orig"]',
+        description:
+          "Наушники Sony WH-1000XM4 - беспроводные шумоподавляющие наушники с превосходным качеством звука и долгим временем работы от аккумулятора.",
+      },
+      {
+        id: "c75acd48-f0cf-4589-896a-daada400e894",
+        name: "Камера Canon EOS R5",
+        category_id: "ce1eb93d-22e1-4a4f-8e99-2adc7496a234",
+        user_id: "7fe6128c-60c6-4af9-bc89-9c12d9ef5e5d",
+        price: "399990",
+        discount: 0,
+        reviews: "[]",
+        rating: 4,
+        product_quantity: 5,
+        image:
+          '["https://avatars.mds.yandex.net/get-mpic/1866068/img_id2287277743134659001.png/orig"]',
+        description:
+          "Камера Canon EOS R5 - профессиональная беззеркальная камера с высоким разрешением и множеством возможностей для фотографии и видеосъемки.",
+      },
+      {
+        id: "ad694618-f86f-4d1c-a0a3-397535ff4b7c",
+        name: "Смартфон Samsung Galaxy S21",
+        category_id: "ce1eb93d-22e1-4a4f-8e99-2adc7496a234",
+        user_id: "44b75e7f-07c0-43be-aa93-757f771bbd45",
+        price: "69999",
+        discount: 10,
+        reviews:
+          '[{"id_user": "9cfa5aa3-168b-4d25-bb01-8ea44a593eb7", "value": 9, "comment": "Отличное качество звука!"}]',
+        rating: 4,
+        product_quantity: 50,
+        image:
+          '["https://avatars.mds.yandex.net/get-mpic/5233681/img_id6187862100290802397.jpeg/orig"]',
+        description:
+          "Смартфон Samsung Galaxy S21 - мощный и современный смартфон с высокой производительностью, отличным дисплеем и передовыми функциями.",
+      },
+      {
+        id: "de0bee78-8812-4dca-a43e-25720ef46cd6",
+        name: "Ноутбук Apple MacBook Pro",
+        category_id: "ce1eb93d-22e1-4a4f-8e99-2adc7496a234",
+        user_id: "99313318-d59d-4e1f-b218-3c81d2a01395",
+        price: "129999",
+        discount: 0,
+        reviews:
+          '[{"id_user": "9cfa5aa3-168b-4d25-bb01-8ea44a593eb7", "value": 9, "comment": "Отличное качество звука!"}]',
+        rating: 4,
+        product_quantity: 30,
+        image:
+          '["https://avatars.mds.yandex.net/get-mpic/5242361/img_id2420073412246465223.jpeg/orig"]',
+        description:
+          "Ноутбук Apple MacBook Pro - надежный и мощный ноутбук с высокой производительностью, стильным дизайном и продолжительным временем автономной работы.",
+      },
+      {
+        id: "6c526f82-0505-4bd5-bbd9-0c450e668edc",
+        name: "Телевизор LG OLED CX",
+        category_id: "ce1eb93d-22e1-4a4f-8e99-2adc7496a234",
+        user_id: "7fe6128c-60c6-4af9-bc89-9c12d9ef5e5d",
+        price: "99999",
+        discount: 15,
+        reviews:
+          '[{"id_user": "9cfa5aa3-168b-4d25-bb01-8ea44a593eb7", "value": 9, "comment": "Отличное качество звука!"}]',
+        rating: 4,
+        product_quantity: 20,
+        image:
+          '["https://avatars.mds.yandex.net/get-mpic/4250892/img_id3331157167952808236.jpeg/orig"]',
+        description:
+          "Телевизор LG OLED CX - высококачественный телевизор с технологией OLED, великолепным качеством изображения и поддержкой современных функций Smart TV.",
+      },
+      {
+        id: "8dd32056-d7f7-4b81-b998-37f8d930b99f",
+        name: "Наушники Sony WH-1000XM4",
+        category_id: "ce1eb93d-22e1-4a4f-8e99-2adc7496a234",
+        user_id: "7fe6128c-60c6-4af9-bc89-9c12d9ef5e5d",
+        price: "24999",
+        discount: 20,
+        reviews:
+          '[{"id_user": "9cfa5aa3-168b-4d25-bb01-8ea44a593eb7", "value": 9, "comment": "Отличное качество звука!"}]',
+        rating: 4,
+        product_quantity: 100,
+        image:
+          '["https://avatars.mds.yandex.net/get-mpic/5345452/img_id6879468432363438497.jpeg/orig"]',
+        description:
+          "Наушники Sony WH-1000XM4 - беспроводные наушники с активным шумоподавлением, высоким качеством звука и долгим временем работы от одного заряда.",
+      },
+      {
+        id: "0f24d7f3-fd66-4769-9419-c1050a1587d3",
+        name: "Фотоаппарат Canon EOS R5",
+        category_id: "ce1eb93d-22e1-4a4f-8e99-2adc7496a234",
+        user_id: "44b75e7f-07c0-43be-aa93-757f771bbd45",
+        price: "329999",
+        discount: 0,
+        reviews: "[]",
+        rating: 4,
+        product_quantity: 20,
+        image:
+          '["https://avatars.mds.yandex.net/get-mpic/5220932/img_id7340363232952078748.jpeg/orig"]',
+        description:
+          "Фотоаппарат Canon EOS R5 - профессиональная камера с высоким разрешением, быстрым автофокусом и возможностью съемки видео в 8K.",
+      },
+      {
+        id: "22fef140-85ac-42ee-b983-e47cff1a64cd",
+        name: "Портативная колонка JBL Charge 4",
+        category_id: "8762c01c-367f-4dfe-8811-99a09b860f7e",
+        user_id: "99313318-d59d-4e1f-b218-3c81d2a01395",
+        price: "5999",
+        discount: 10,
+        reviews:
+          '[{"id_user": "9cfa5aa3-168b-4d25-bb01-8ea44a593eb7", "value": 9, "comment": "Отличное качество звука!"}]',
+        rating: 4,
+        product_quantity: 50,
+        image:
+          '["https://avatars.mds.yandex.net/get-mpic/5312993/img_id683095104744541357.jpeg/orig",]',
+        description:
+          "Портативная колонка JBL Charge 4 - мощная колонка с отличным качеством звука, водонепроницаемым корпусом и долгим временем работы от аккумулятора.",
+      },
+      {
+        id: "684bd7ae-453c-46c1-95ed-3fd64c15737a",
+        name: "Смарт-часы Apple Watch Series 6",
+        category_id: "ce1eb93d-22e1-4a4f-8e99-2adc7496a234",
+        user_id: "7fe6128c-60c6-4af9-bc89-9c12d9ef5e5d",
+        price: "19999",
+        discount: 0,
+        reviews: "[]",
+        rating: 4,
+        product_quantity: 30,
+        image:
+          '["https://avatars.mds.yandex.net/get-mpic/5391389/img_id1002833500338952495.png/orig"]',
+        description:
+          "Смарт-часы Apple Watch Series 6 - элегантные и функциональные часы с ярким дисплеем, множеством датчиков и возможностью отслеживания физической активности.",
+      },
+      {
+        id: "76d6d95e-07c0-4ae1-8fd2-82d1f94a864b",
+        name: "Игровая консоль PlayStation 5",
+        category_id: "8762c01c-367f-4dfe-8811-99a09b860f7e",
+        user_id: "99313318-d59d-4e1f-b218-3c81d2a01395",
+        price: "44999",
+        discount: 5,
+        reviews:
+          '[{"id_user": "9cfa5aa3-168b-4d25-bb01-8ea44a593eb7", "value": 9, "comment": "Отличное качество звука!"}]',
+        rating: 4,
+        product_quantity: 10,
+        image:
+          '["https://avatars.mds.yandex.net/get-mpic/5068955/img_id6047884633257749435.jpeg/orig"]',
+        description:
+          "Игровая консоль PlayStation 5 - мощная и инновационная консоль нового поколения с поддержкой 4K-графики, высокой скоростью загрузки и широким каталогом игр.",
+      },
     ]);
   },
 
